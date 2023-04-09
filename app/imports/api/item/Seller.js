8df86dedc578311f8aeb4005d91220d9423002f0
Jazmin Lor <lorj@hawaii.edu>
2023-04-08 15:21:34 -1000

    Updated branch name

--- app/imports/api/item/Seller.js
+++ app/imports/api/item/Seller.js
@@ -19,7 +19,6 @@ class SellersCollection {
       description: String,
       owner: String,
     });
-    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
     this.collection.attachSchema(this.schema);
     // Define names for publications and subscriptions
     this.userPublicationName = `${this.name}.publication.user`;
