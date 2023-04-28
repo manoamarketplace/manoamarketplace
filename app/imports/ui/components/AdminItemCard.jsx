fb87ab0be8efd54e476b9c600b1d225190726272
Jing Zheng <116461363+jingtzheng@users.noreply.github.com>
2023-04-27 14:58:05 -1000

    Update AdminItemCard.jsx

--- app/imports/ui/components/AdminItemCard.jsx
+++ app/imports/ui/components/AdminItemCard.jsx
@@ -1,36 +1,46 @@
 /* Component for layout out a item Card. */
 import React from 'react';
-import { Card, Col, Image, Row } from 'react-bootstrap';
+import { Card, Col, Image, Button, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import DeleteItem from './DeleteItem';
+import { Trash } from 'react-bootstrap-icons';
 
-const ItemCard = ({ item }) => (
-  <Col>
-    <Card className="h-100">
-      <Card.Header>
-        <Image src={item.image} width={325} height={186} />
-        <Card.Title><h2>{item.name}</h2></Card.Title>
-        <Card.Subtitle>${item.price}</Card.Subtitle>
-      </Card.Header>
-      <Card.Body>
-        <Card.Text>{item.seller}</Card.Text>
-        <Card.Text>{item.condition}</Card.Text>
-        <Card.Text>
-          {item.description}
-        </Card.Text>
-        {item.reported ? ([
-          <p>Item has been reported!</p>,
-        ]) : ([
-        ])}
-      </Card.Body>
-      <Card.Footer>
-        <Row><DeleteItem item={item} /></Row>
-      </Card.Footer>
-    </Card>
-  </Col>
-);
+const AdminItemCard = ({ item, collection }) => {
+  const removeItem = (docId) => {
+    collection.remove(docId);
+  };
+  return (
+    <Col>
+      <Card className="h-100">
+        <Card.Header>
+          <Image src={item.image} width={325} height={186} />
+          <Row>
+            <Col>
+              <Card.Title><h2>{item.name}</h2></Card.Title>
+              <Card.Subtitle>${item.price}</Card.Subtitle>
+            </Col>
+            <Col xs={2} className="text-left py-2">
+              <Button variant="danger" onClick={() => removeItem(item._id)}><Trash />
+              </Button>
+            </Col>
+          </Row>
+        </Card.Header>
+        <Card.Body>
+          <Card.Text>{item.seller}</Card.Text>
+          <Card.Text>{item.condition}</Card.Text>
+          <Card.Text>
+            {item.description}
+          </Card.Text>
+          {item.reported ? ([
+            <p>Item has been reported!</p>,
+          ]) : ([
+          ])}
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
-ItemCard.propTypes = {
+AdminItemCard.propTypes = {
   item: PropTypes.shape({
     name: PropTypes.string,
     category: PropTypes.string,
@@ -40,6 +50,10 @@ ItemCard.propTypes = {
     condition: PropTypes.string,
     seller: PropTypes.string,
     reported: PropTypes.bool,
+    _id: PropTypes.string,
   }).isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  collection: PropTypes.object.isRequired,
 };
-export default ItemCard;
+
+export default AdminItemCard;
