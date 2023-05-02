e949c11935e525bf82898f787f1913a24a4e2baf
Jing Zheng <116461363+jingtzheng@users.noreply.github.com>
2023-05-02 09:35:36 -1000

    Created a test for more info page

--- app/imports/ui/pages/Categories.jsx
+++ app/imports/ui/pages/Categories.jsx
@@ -12,7 +12,7 @@ const CategoriesPage = () => (
         <Row className="justify-content-md-center">
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/textbooks" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/books.jpg" width={325} height={186} />
                   <h2>Textbooks</h2>
@@ -22,7 +22,7 @@ const CategoriesPage = () => (
           </Col>
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/stationary" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/stationary.jpg" width={325} height={186} />
                   <h2>Stationary</h2>
@@ -32,7 +32,7 @@ const CategoriesPage = () => (
           </Col>
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/electronics" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/electronics.jpeg" width={325} height={186} />
                   <h2>Electronics</h2>
@@ -44,7 +44,7 @@ const CategoriesPage = () => (
         <Row className="justify-content-md-center">
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/bathroom" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/bath.png" width={325} height={186} />
                   <h2>Bathroom</h2>
@@ -54,7 +54,7 @@ const CategoriesPage = () => (
           </Col>
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/kitchen" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/kitchen.jpg" width={325} height={186} />
                   <h2>Kitchen</h2>
@@ -64,7 +64,7 @@ const CategoriesPage = () => (
           </Col>
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/gym" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/gym.jpg" width={325} height={186} />
                   <h2>Gym</h2>
@@ -76,7 +76,7 @@ const CategoriesPage = () => (
         <Row className="justify-content-md-center">
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/transportation" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/transportation.jpg" width={325} height={186} />
                   <h2>Transportation</h2>
@@ -86,7 +86,7 @@ const CategoriesPage = () => (
           </Col>
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/dorm" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/dorm2.png" width={325} height={186} />
                   <h2>Dorm Essentials</h2>
@@ -96,7 +96,7 @@ const CategoriesPage = () => (
           </Col>
           <Col xs="auto" className="py-3">
             <Col className="text-center">
-              <Card>
+              <Card id="individual-category">
                 <Link to="/list/clothing" style={{ color: 'forestgreen', textDecoration: 'none' }}>
                   <Image src="/images/clothes.jpg" width={325} height={186} />
                   <h2>Clothing</h2>
